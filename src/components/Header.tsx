19d37e0c800491330064e6249cfc12c3cd821fbc
Mark Adrian <markadrian.229@gmail.com>
2024-11-05 21:16:07 +0800

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <div className="h-12 bg-slate-600">
+            Header Here!
+        </div>
+    );
+}
+
+export default Header;
